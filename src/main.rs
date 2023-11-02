795ec5dd4657baeb662d471fd54ad9758e4eb5c6
CoCo_Sol <solois.corentin@gmail.com>
2023-11-02 20:50:51 +0100

    html

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
 //! The main program of my website.
 
-#![deny(clippy::missing_docs_in_private_items)]
-// #![deny(clippy::unwrap_in_result)]
-#![deny(clippy::unwrap_used)]
-#![deny(clippy::nursery)]
-#![deny(missing_docs)]
-#![deny(clippy::all)]
-#![deny(warnings)]
-
-mod id_gen;
-use id_gen::*;
-
 use once_cell::sync::Lazy;
-use rocket::form::FromForm;
+use rocket::form::{Form, FromForm};
+use rocket::fs::TempFile;
+use rocket::http::{Cookie, CookieJar};
+use rocket::serde::uuid::Uuid;
 use rocket::{
     get, http::ContentType, launch, post, routes, serde::json::Json, tokio::sync::RwLock, Config,
     Route,
 };
 use serde::{Deserialize, Serialize};
+use sha1::{Digest, Sha1};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::Write;
+use std::io::{self, Write};
 use std::vec;
 use tokio::spawn;
 
@@ -42,13 +35,16 @@ macro_rules! raw_files {
 }
 
 raw_files! {
-    "/wave" => wave(SVG, "webpages/wave.svg"),
+    "/background" => wave(SVG, "webpages/background.svg"),
     "/style" => style(CSS, "webpages/style.css"),
-    "/" => visualize(HTML, "test.html"),
+    "/" => main_page(HTML, "webpages/main.html"),
+    "/about_me" => aboute_me_page(HTML, "webpages/about_me.html"),
+    "/articles" => articles_page(HTML, "webpages/articles.html"),
+    "/images" => images_page(HTML, "webpages/images.html"),
 }
 
 /// a struct to store articles.
-static ARTICLES: Lazy<RwLock<HashMap<u64, Article>>> = Lazy::new(|| RwLock::new(HashMap::new()));
+static ARTICLES: Lazy<RwLock<HashMap<Uuid, Article>>> = Lazy::new(|| RwLock::new(HashMap::new()));
 
 /// The struct to represent an article.
 #[derive(Serialize, Deserialize, Clone, Debug, FromForm)]
@@ -58,11 +54,12 @@ struct Article {
     /// The introduction of the article.
     intro: String,
     /// The content of the article.
-    content: String,
+    content: Vec<String>,
 }
+
 /// a function to get an article with its id.
 #[get("/article/<id>")]
-async fn get_article(id: u64) -> Option<Json<Article>> {
+async fn get_article(id: Uuid) -> Option<Json<Article>> {
     // On récupère l'accès aux articles qui sont dans un RwLock puis,
     // on récupère l'article et si il existe on le convertis en Json
     // sinon on renvoit None ce qui a pour effet de faire une erreur 404
@@ -75,18 +72,19 @@ async fn get_article(id: u64) -> Option<Json<Article>> {
 
 /// a function to list all articles.
 #[get("/articles")]
-async fn list_articles() -> Json<Vec<u64>> {
+async fn list_articles() -> Json<Vec<Uuid>> {
     Json(ARTICLES.read().await.keys().copied().collect())
 }
 
 /// a function to add an article.
 #[post("/new_article", data = "<article>")]
-async fn add_article(article: Option<rocket::form::Form<Article>>) {
-    if let Some(article) = article {
+async fn add_article(cookies: &CookieJar<'_>, article: Option<rocket::form::Form<Article>>) {
+    if !is_admin(cookies) {
+        println!("error: POST request without grade admin");
+    } else if let Some(article) = article {
         let article = article.into_inner();
         // On cree une nouvelle id.
-        let id = IdGenerator::with_used_ids(ARTICLES.read().await.keys().copied().collect())
-            .generate_unique_id();
+        let id = Uuid::new_v4();
 
         // On stocke l'article dans un la static.
         ARTICLES.write().await.insert(id, article);
@@ -116,12 +114,66 @@ async fn add_article(article: Option<rocket::form::Form<Article>>) {
     }
 }
 
+#[derive(FromForm)]
+struct Upload<'f> {
+    file: TempFile<'f>,
+}
+
+#[post("/upload", format = "multipart/form-data", data = "<form>")]
+async fn upload_image(mut form: Form<Upload<'_>>) -> std::io::Result<()> {
+    let file_name: Option<&str> = form.file.name();
+    if let Some(name) = file_name {
+        let file_name = String::from("static/") + name + ".png";
+
+        form.file.persist_to(file_name).await?;
+
+        Ok(())
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Failed to get file name",
+        ))
+    }
+}
+
+/// a function to login as admin.
+#[get("/login/<password>")]
+async fn login_admin(cookies: &CookieJar<'_>, password: String) {
+    if sha1_hash(&password) == "5ed25af7b1ed23fb00122e13d7f74c4d8262acd8" {
+        // on ajoute le cookie prive
+        cookies.add_private(Cookie::new("admin", "true"));
+    }
+}
+/// a function to hash a string.
+fn sha1_hash(input: &str) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(input);
+    format!("{:x}", hasher.finalize())
+}
+
+/// a fonction to get if the user is admin.
+fn is_admin(cookies: &CookieJar<'_>) -> bool {
+    cookies
+        .get_private("admin")
+        .map_or(false, |cookie| cookie.value() == "true")
+}
+
+/// a function to check if cookie is admin.
+#[get("/check_admin")]
+fn check_admin(cookies: &CookieJar<'_>) -> String {
+    if is_admin(cookies) {
+        "true".to_string()
+    } else {
+        "false".to_string()
+    }
+}
+
 /// The main function of the website.
 #[launch]
 async fn rocket() -> _ {
     let file = File::open("articles.json").expect("Failed to open articles.json");
     // on affichie si il y a une erreur
-    let articles: HashMap<u64, Article> =
+    let articles: HashMap<Uuid, Article> =
         serde_json::from_reader(file).expect("Failed to read articles");
     ARTICLES.write().await.extend(articles.into_iter());
 
@@ -131,5 +183,9 @@ async fn rocket() -> _ {
             ..Default::default()
         })
         .mount("/", raw_routes())
-        .mount("/api", routes![get_article, list_articles, add_article])
+        .mount(
+            "/api",
+            routes![get_article, list_articles, add_article, upload_image],
+        )
+        .mount("/admin", routes![login_admin, check_admin])
 }
